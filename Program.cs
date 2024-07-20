92ff0afd981d194c6fe589bdc2bd36b5e22d3ee5
Yusuf Mert Genc <106550032+YusufMertGenc@users.noreply.github.com>
2024-07-20 23:59:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class BasicMath
+    {
+        public double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double subtract(double num1, double num2) 
+        {
+            return num1 + num2; 
+        }
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BasicMath tm = new BasicMath();
+            double result = tm.add(10, 10);
+            Assert.AreEqual(result, 20);
+        }
+        [TestMethod]
+
+        public void TestMethod2()
+        {
+            BasicMath tm = new BasicMath();
+            double result = tm.subtract(10, 10);  
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
